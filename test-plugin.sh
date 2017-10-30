da4dce74a7b9907c962a9950f6f60ff23fb3fa79
Alex Ellis <alexellis2@gmail.com>
2017-10-30 14:03:08 +0000

    Linting and optional Swarm init
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

--- test-plugin.sh
+++ test-plugin.sh
@@ -1,25 +1,39 @@
 #!/bin/bash
 
+# clear out previous driver / test-run
 rm -rf driver
-mkdir -p driver/
 
+mkdir -p driver/
 cd driver
+
+# Install Node.js driver/template
 serverless install --url https://github.com/aafrey/openfaas-nodejs --name faas-func
 
 cd ..
 
+# Copy in node_modules and faas provider developer version
 cp -r node_modules driver/faas-func/
 cp -r faas driver/faas-func/node_modules/serverless-faas
 
 cd driver/faas-func
 #node --inspect-brk=0.0.0.0:9229 /home/austin/.nvm/versions/node/v8.4.0/bin/sls package
-serverless init
 
+# Run the workflow
+
+serverless init
 
 serverless package
 serverless deploy
+
 sleep 10
+
 serverless invoke -f hello-serverless
+
 serverless deploy function -f hello-serverless
+
 serverless deploy list
+
+serverless invoke -f hello-serverless
+
+# Comment out to leave in place.
 serverless remove
